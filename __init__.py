1426a8513850993938247eadfb4bf506ee12daea
BLINMATIC <68463606+BLINMATIC@users.noreply.github.com>
2022-12-12 20:47:32 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from . import time
-from . import control
-from . import draw
-from . import collision
-from .display import Screen
-
-from . import _metadata
-print("Graphite ", _metadata.codename, _metadata.version, "--", _metadata.release_date)
